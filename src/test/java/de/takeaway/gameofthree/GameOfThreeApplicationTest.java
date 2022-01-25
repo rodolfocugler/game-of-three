7ea2e6757ff9f8244f7991b100fd9c2102e0ce03
Rodolfo Cugler <rodolfocugler@outlook.com>
2022-01-25 14:50:21 +0100

    Adding maven configuration and initial spring boot setup

---
+++ src/test/java/de/takeaway/gameofthree/GameOfThreeApplicationTest.java
@@ -0,0 +1,13 @@
+package de.takeaway.gameofthree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameOfThreeApplicationTest {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
