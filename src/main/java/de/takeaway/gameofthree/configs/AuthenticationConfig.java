9ad2dcf5ef43f16fbd9eccfcc326a55afd311deb
Rodolfo Cugler <rodolfocugler@outlook.com>
2022-01-26 16:42:20 +0100

    Getting JWT Secret from environment

--- src/main/java/de/takeaway/gameofthree/configs/AuthenticationConfig.java
+++ src/main/java/de/takeaway/gameofthree/configs/AuthenticationConfig.java
@@ -1,5 +1,6 @@
 package de.takeaway.gameofthree.configs;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -7,7 +8,13 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 @Configuration
 public class AuthenticationConfig {
 
-  public static final String SECRET = "0a046f598da583324875d93ff23f3657d4e6c025336bd08dc02f609878149b4fb6560829da98f4c28a90323458799011d1888b46c5096b7428c24a5cc63f8308";
+  public static String SECRET;
+
+  @Value("${application.secret}")
+  public void setSecretStatic(String secret) {
+    AuthenticationConfig.SECRET = secret;
+  }
+
   public static final long EXPIRATION_TIME = 3600000;
   public static final String TOKEN_PREFIX = "Bearer ";
   public static final String HEADER_STRING = "Authorization";
